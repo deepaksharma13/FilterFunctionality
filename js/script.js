b409c2181c592b0d1881c0085b2482df7cc4c9ea
Deepak Sharma <deepakfrontend04@gmail.com>
2024-07-05 13:53:42 +0530

    unused code removed

--- js/script.js
+++
@@ -1,6 +0,0 @@
-let filterSpan = document.getElementById("filterSpan");
-filterSpan.addEventListener("click", () => {
-  let hideSec = document.querySelectorAll(".hideSec");
-  //hideSec.classList.toggle("d-none");
-  hideSec.forEach(x => x.classList.toggle('d-block'))
-});
